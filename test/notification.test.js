095f99bb46ea6af5c6414111dc8f234c2304bb68
igorPhelype <igor.appmasters@gmail.com>
2018-03-08 10:19:35 -0300

    changing test and updating readme

--- test/notification.test.js
+++ test/notification.test.js
@@ -34,27 +34,10 @@ test('Send notification', async () => {
     const userModel = mongoose.model('user');
     let user = null;
     await userModel.findOne({_id: '5a7ae1a0f9305b00145344f1'}).then((response) => user = response).catch(error => console.error(error));
-    const users = {
-        _id: '59cf9ca299fa324ffff06c13',
-        notification: {
-            ios: {
-                token: []
-            },
-            web: {
-                token: [
-                    "doetStMdwvA:APA91bHf7wGssOwfkLi6FyNEnlXqEK1MXmM3f5rFMEUceUlWg7D9nb9M-XSJaGExhcDd59EmnE13KKoLGl74JIeW0-zTfAvnvZzKoSAwsybuxwCW4HZS8QsDeRTfuJPcY0nnYPgLeFHw",
-                    "dJsfffhz4-U:APA91bGgfmUT2FFksmKJE98D0TxgTa1HRS29p-Sl8I14PI6iAWAoOeDmy-OI4EwXiNHxUdEJTrWfyufwK968ReF6zeF7JMky23d7Si8S4F4mQ6WjnjhfwFYQuVEF5rRJH65fz6PS3nyk"
-                ]
-            },
-            android: {
-                token: []
-            }
-        }
-    };
     let payload = {
         notification: {
-            title: '183.58333333333334 alocado na issue',
-            body: 'Você ultrapassou o limite da issue!'
+            title: 'My test - title',
+            body: 'Body'
         }
     };
     notification.send(user, payload);
