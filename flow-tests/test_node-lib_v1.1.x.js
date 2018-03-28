ab87d2a7398d94109a53b9f9255432fe4abb1786
Bruno Ferreira Cangussu <bruno.cangussu@hotmail.com>
2018-03-28 15:22:16 -0300

    Flow adicionado para modelSequelize

--- flow-tests/test_node-lib_v1.1.x.js
+++ flow-tests/test_node-lib_v1.1.x.js
@@ -3,40 +3,44 @@ import { ModelSequelize } from '@app-masters/node-lib';
 import type { QueryS } from '@app-masters/node-lib';
 
 type userSchema = {
-  name: string | QueryS,
-  age: number | QueryS,
-  data: {
-    active: boolean
-  } | QueryS,
+	name: string | QueryS,
+	age: number | QueryS,
+	data: {
+		active: boolean | QueryS
+	},
 }
 type options = {
-  created_at: Date,
-
+	created_at: string,
 }
 
-class User extends ModelSequelize<userSchema> {
-  getActives () {
-    return User.find({ data: { active: true } }, 'role', 'name');
-  }
+class User extends ModelSequelize<userSchema, options> {
+	getActives () {
+		return User.find({ 'data.active': true }, 'role', 'name');
+	}
 }
 class UserInstance {
-  activate (): void {
-    //$FlowFixMe, 'this' will only exits after setup runs, but this is correct
-    this.data.active = true;
-  }
+	activate (): void {
+		//$FlowFixMe, 'this' will only exits after setup runs, but this is correct
+		this.data.active = true;
+	}
 }
+
 User.setup({}, 'user', { name: String, age: Number, data: { active: Boolean } }, UserInstance);
+
+//$FlowFixMe, Age must be a number
 User.create({
-  name: 'joao',
-  tes: 'ta'
+	name: 'Carl',
+	age: '12'
 });
 
 User.find({ age: { $gte: 15 } }).then(r => {
-  if (r) {
-    var a = r[0];
-    if(a.data)
-    {
-      console.log(a.data.active);
-    }
-  }
+	// $FlowFixMe Error cause you didn't check for when R is null
+	var a = r[0];
+	console.log(a.data.active);
+});
+
+User.find({name: 'claudio'}).then(r => {
+	if(r) {
+		console.log(r[0].created_at);
+	}
 })
--- flow-typed/node-lib_v1.1.x.js
+++ flow-typed/node-lib_v1.1.x.js
@@ -4,13 +4,14 @@ type NodeLib$QueryS = Object;
 declare class NodeLib$ModelSequelize<Schema, Options>{
   constructor (): this;
   static setup (sequelize: Object, modelName: string, schema: Object, instance: Object, relationArray: ?Array<NodeLib$ModelSequelize<any, any>>): void;
-  static create (obj: {...Schema}): Promise<Object>;
-  static find (where: {...Schema}, include: ?string, order: ?string): Promise<?Array<{...Schema, ...Options}>>;
-  static findById (id: ObjectId, include: ?string): Promise<?{...Schema, ...Options}>;
-  static findOne (where: {...Schema}, include: ?string): Promise<?{...Schema, ...Options}>;
+  static create (obj: {...Schema}): Promise<$Exact<Schema> & $Exact<Options>>;
+  static find (where: {...Schema}, include: ?string, order: ?string): Promise<?Array<$Exact<Schema> & $Exact<Options>>>;
+  static findById (id: ObjectId, include: ?string): Promise<?($Exact<Schema> & $Exact<Options>)>;
+  static findOne (where: {...Schema}, include: ?string): Promise<?$Exact<Schema> & $Exact<Options>>;
   static update (where: {...Schema}, obj: Object): Promise<boolean>;
   static delete (where: {...Schema}): Promise<boolean>;
 }
+
 declare class NodeLib$ModelMongoose<Schema>{
   constructor (): this;
   static setup (modelName: string, schema: Object, instance: Object, relationArray: ?Array<NodeLib$ModelSequelize<any>>): void;
@@ -21,6 +22,7 @@ declare class NodeLib$ModelMongoose<Schema>{
   static update (where: Object, obj: Object): Promise<boolean>;
   static delete (where: Object): Promise<boolean>;
 }
+
 declare module '@app-masters/node-lib' {
   declare export type NodeLibModelS<Schema> = NodeLib$ModelSequelize<Schema>;
   declare export type QueryS = NodeLib$QueryS;
