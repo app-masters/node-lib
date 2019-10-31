10d18288bfbe6036fac906830fa43b921437e694
Tiago Gouvêa <tiago@tiagogouvea.com.br>
2019-10-31 14:51:29 -0300

    Platform version check updated to handle ios and android versions

--- test/apiBootstrap.test.js
+++ test/apiBootstrap.test.js
@@ -1,27 +1,25 @@
-
 //   Requires
-var express = require('express');
+const express = require('express');
 const passport = require('passport');
 const packag = require('../package');
-var envs = require('./src/testConfig');
-var app = express();
+const envs = require('./src/testConfig');
+const app = express();
 const apiBootstrap = require('../lib/apiBootstrap');
-const AMMailing = require('../lib/am-mailing');
+// const AMMailing = require('../lib/am-mailing');
 require('./src/userSchema');
 require('./src/inviteSchema');
 require('./src/notificationSchema');
 
-const testEmail = 'baraky.appmasters@gmail.com';
-
 test('ApiBootstrap node-lib', () => {
     let resultSetup = apiBootstrap.setup(app, envs, packag, passport);
+    expect(resultSetup).toBe(true);
     // 3 - Listen API
     let resultListem = apiBootstrap.listen(app);
-    expect(resultSetup).toBe(true);
     expect(resultListem).toBe(true);
 });
-//
+
 // test('SendEmail node-lib', async () => {
+// const testEmail = 'baraky.appmasters@gmail.com';
 //     const code = '123123123';
 //     const HTML = `<h1>WILLIM</h1> <p>Código para redefinir senha: ${code}</p>`;
 //     const MESSAGE = `Código para redefinir senha: ${code}`;
